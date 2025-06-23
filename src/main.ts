a7422f34d43f284b3b2b011cdfe0854db5d270dd
gaeunchoi <gaeun1884@naver.com>
2025-06-23 15:03:10 +0900

    Feat: CORS 에러 해결

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: 'GET,POST,PATCH,PUT,DELETE,OPTIONS',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
