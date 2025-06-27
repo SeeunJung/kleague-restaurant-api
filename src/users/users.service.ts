0d62c48dad6b76b98c71e8ae5eb26b02cf5c218d
gaeunchoi <gaeun1884@naver.com>
2025-06-27 22:41:47 +0900

    Fix: 회원정보 수정 시 중복 휴대폰번호 방지

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -25,28 +29,56 @@ export class UsersService {
   async findById(userId: number) {
     const user = await this.prisma.user.findUnique({
       where: { id: userId },
-      select: {
-        id: true,
-        username: true,
-        nickname: true,
-        email: true,
-        phoneNumber: true,
-        favoriteTeam: true,
-        createdAt: true,
-        updatedAt: true,
+      include: {
+        favorites: {
+          include: {
+            restaurant: {
+              select: {
+                id: true,
+                name: true,
+                category: true,
+                address: true,
+                stadiumId: true,
+              },
+            },
+          },
+        },
+        reviews: {
+          include: {
+            restaurant: {
+              select: {
+                id: true,
+                name: true,
+                category: true,
+              },
+            },
+          },
+          orderBy: { createdAt: 'desc' },
+        },
       },
     });
 
     if (!user) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
-    return user;
+
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 
   // 유저 업데이트 (me 기준)
   async update(userId: number, dto: UpdateUserDto) {
     const user = await this.prisma.user.findUnique({ where: { id: userId } });
     if (!user) throw new NotFoundException('사용자를 찾을 수 없습니다.');
+
+    if (dto.phoneNumber) {
+      const phoneUsed = await this.prisma.user.findFirst({
+        where: { phoneNumber: dto.phoneNumber, id: { not: userId } },
+      });
+
+      if (phoneUsed)
+        throw new ConflictException('해당 번호는 이미 등록된 전화번호입니다.');
+    }
 
     return this.prisma.user.update({
       where: { id: userId },
